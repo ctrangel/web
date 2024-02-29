e9eba0139879d0b5c7912ab41c73190a0ea3ce6a
ctrangel <122327961+ctrangel@users.noreply.github.com>
2024-02-29 17:43:56 -0500

    ADD: setTimer

--- js/login.js
+++ js/login.js
@@ -35,6 +35,8 @@ $(document).ready(function () {
     var username = $("#loginUsername").val();
     var password = $("#loginPassword").val();
 
+    $("#loader").show();
+
     $.ajax({
       // url: "https://inventory-b7qi.onrender.com/api/v1/inventory/login",
       url: "http://localhost:8003/api/v1/inventory/login",
@@ -42,16 +44,24 @@ $(document).ready(function () {
       contentType: "application/json",
       data: JSON.stringify({ username: username, password: password }),
       success: function (response) {
+        toastr.success("Your bluetooth device is paired successfully");
         localStorage.setItem("authToken", response.token);
-        window.location.href = "/inventory.html";
-        toastr.success('Your bluetooth device is paried uh sucessfully');
+        // Delay redirect to allow toastr message to be seen
+        setTimeout(function () {
+          window.location.href = "/inventory.html";
+        }, 2000); 
       },
       error: function (xhr, status, error) {
         if (xhr.status == 404) {
           toastr.error("Login endpoint not found. Please check the URL.");
+        } else if (xhr.status == 401) {
+          toastr.error("username or password is incorrect. Please try again.");
         } else {
           toastr.error("Login failed: " + xhr.responseText || error);
         }
+      },
+      complete: function () {
+        $("#loader").hide();
       },
     });
   });
@@ -64,13 +74,16 @@ $(document).ready(function () {
     var password = $("#registerPassword").val();
 
     $.ajax({
-      url: "http://localhost:8003/api/v1/inventory/register", 
+      url: "http://localhost:8003/api/v1/inventory/register",
       type: "POST",
       contentType: "application/json",
       data: JSON.stringify({ username: username, password: password }),
       success: function (response) {
         toastr.success("Registration successful. Please log in.");
         // Optionally redirect to the login page or automatically log the user in
+        setTimeout(() => {
+          window.location.href = "/index.html";
+        }, 1000);
       },
       error: function (xhr, status, error) {
         toastr.error("Registration failed: " + xhr.responseText || error);
